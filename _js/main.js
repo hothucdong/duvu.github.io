fa5f2201a86a420df735b3902617dff3f6b85bd1
Ha Pham <ha.fit.hut@gmail.com>
2014-12-03 18:17:59 +0700

    :monkey:

--- _js/main.js
+++ _js/main.js
@@ -1,6 +1,33 @@
 var $ = require('jquery');
-require('foundation');
+require('jquery-waypoints');
+//var Modernizr = require('modernizr');
 
-(function (argument) {
-	$(document).foundation();
+(function () {
+
+	var onepage = function () {
+		var pages = $('.main .section');
+		$.each(pages, function (idx) {
+			var page = $(this);
+			page.css({
+				top: idx*100 + '%'
+			});
+			console.log(page[0]);
+
+		});
+		pages.waypoint(function (direction) {
+			console.log(direction);
+		});
+	};
+
+	var paralaxScroll = function (e) {
+
+	};
+
+	var ready = function () {
+		var main = $('.main');
+		//onepage();
+		main.scroll(paralaxScroll);
+	};
+
+	$(document).ready(ready);
 })();
